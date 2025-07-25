6235b7a2af9e21bf14b232b8e42a32c9c8f87bcf
Mohanad Tarek <mohanad8t@gmail.com>
2025-07-25 04:26:02 +0300

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const config = require('./config.json');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(config.token);
+
+(async () => {
+  try {
+    console.log(`🔁 Refreshing ${commands.length} application (/) commands...`);
+    await rest.put(
+      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      { body: commands }
+    );
+    console.log('✅ Successfully reloaded commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
